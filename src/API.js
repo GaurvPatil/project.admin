10a6a6f77807af910708fe832a6c345b5ba40d86
Gaurav Patil <gaurvpatil17@gmail.com>
2022-10-23 21:25:25 +0530

    heroku deploy 1

--- src/API.js
+++ src/API.js
@@ -1,1 +1,6 @@
-export const API = "http://localhost:5000"
+
+// Local API link 
+// export const API = "http://localhost:5000"
+
+// Heroku API link 
+export const API = "https://project-management-admin.herokuapp.com"
