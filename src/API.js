7bb7fc506788b6adc488c659740e916c8254e387
Gaurav Patil <gaurvpatil17@gmail.com>
2022-10-24 09:05:15 +0530

    API link updated

--- src/API.js
+++ src/API.js
@@ -3,4 +3,4 @@
 // export const API = "http://localhost:5000"
 
 // Heroku API link 
-export const API = "https://project-management-admin.herokuapp.com"
+export const API = "https://project-managementt-api.herokuapp.com"
